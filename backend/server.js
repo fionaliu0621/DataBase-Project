a83fb4031e8c440ad178216db828b9b9a946a972
ChiuVicky <85924779+ChiuVicky@users.noreply.github.com>
2026-06-15 23:17:12 +0800

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -8,6 +8,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+//+
+const authRoutes = require("./routes/authRoutes");
+app.use("/api/auth", authRoutes);
+
 // Products
 app.get('/products', async (req, res) => {
     try {
