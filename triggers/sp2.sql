5ee0ab970c9f1f437d8a3167de1c010197eb558a
fionaliu0621 <52523171+fionaliu0621@users.noreply.github.com>
2026-06-15 19:30:11 +0800

    Update sp2.sql

--- triggers/sp2.sql
+++ triggers/sp2.sql
@@ -1,3 +1,5 @@
+USE railway;
+
 DELIMITER $$
 
 CREATE PROCEDURE GetSellerRevenue(
@@ -8,13 +10,13 @@ BEGIN
         s.seller_id,
         s.seller_city,
         COUNT(oi.order_id) AS total_orders,
-        SUM(oi.price * oi.quantity) AS total_price,
+        SUM(oi.price * oi.order_item_quantity) AS total_price,
         SUM(oi.freight_value) AS total_freight,
-        SUM(oi.price * oi.quantity + oi.freight_value) AS total_revenue
+        SUM(oi.price * oi.order_item_quantity + oi.freight_value) AS total_revenue
     FROM Sellers s
-    JOIN Order_Items oi ON s.seller_id = oi.seller_id
-    JOIN Orders o ON oi.order_id = o.order_id
-    WHERE s.seller_id = p_seller_id
+    JOIN Order_Items oi ON s.seller_id COLLATE utf8mb4_unicode_ci = oi.seller_id COLLATE utf8mb4_unicode_ci
+    JOIN Orders o ON oi.order_id COLLATE utf8mb4_unicode_ci = o.order_id COLLATE utf8mb4_unicode_ci
+    WHERE s.seller_id COLLATE utf8mb4_unicode_ci = p_seller_id COLLATE utf8mb4_unicode_ci
     AND o.order_status = 'delivered'
     GROUP BY s.seller_id, s.seller_city;
 END$$
