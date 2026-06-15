43c9570b8e0e993371bd840d33c33f0f6ab7118b
Ting-An Hung <annhung323451@gmail.com>
2026-06-15 21:25:58 +0800

    fix port

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,27 +7,27 @@ export default defineConfig({
     historyApiFallback: true,
     proxy: {
       '/products': {
-        target: 'https://database-project-production-aefc.up.railway.app:3000',
+        target: 'https://database-project-production-aefc.up.railway.app',
         changeOrigin: true,
       },
       '/orders': {
-        target: 'https://database-project-production-aefc.up.railway.app:3000',
+        target: 'https://database-project-production-aefc.up.railway.app',
         changeOrigin: true,
       },
       '/sellers': {
-        target: 'https://database-project-production-aefc.up.railway.app:3000',
+        target: 'https://database-project-production-aefc.up.railway.app',
         changeOrigin: true,
       },
       '/payments': {
-        target: 'https://database-project-production-aefc.up.railway.app:3000',
+        target: 'https://database-project-production-aefc.up.railway.app',
         changeOrigin: true,
       },
       '/reviews': {
-        target: 'https://database-project-production-aefc.up.railway.app:3000',
+        target: 'https://database-project-production-aefc.up.railway.app',
         changeOrigin: true,
       },
       '/customers': {
-        target: 'https://database-project-production-aefc.up.railway.app:3000',
+        target: 'https://database-project-production-aefc.up.railway.app',
         changeOrigin: true,
       },
     }
