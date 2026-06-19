9806a023e6ab0f48add8fd26b20da91f834c9947
fionaliu0621 <52523171+fionaliu0621@users.noreply.github.com>
2026-06-19 18:01:49 +0800

    Update trigger1.sql

--- triggers/trigger1.sql
+++ triggers/trigger1.sql
@@ -1,18 +1,6 @@
-USE railway;
-
-DELIMITER $$
-
-CREATE TRIGGER trg_order_status_log
-AFTER UPDATE ON Orders
-FOR EACH ROW
-BEGIN
-    IF OLD.order_status <> NEW.order_status THEN
-        INSERT INTO Order_Status_Log (
-            order_id, old_status, new_status
-        ) VALUES (
-            NEW.order_id, OLD.order_status, NEW.order_status
-        );
+CREATE DEFINER=`root`@`%` TRIGGER `trg_order_status_log` AFTER UPDATE ON `Orders` FOR EACH ROW BEGIN
+    IF OLD.order_status != NEW.order_status THEN
+        INSERT INTO Order_Status_Log(order_id, old_status, new_status)
+        VALUES (OLD.order_id, OLD.order_status, NEW.order_status);
     END IF;
-END$$
-
-DELIMITER ;
+END
