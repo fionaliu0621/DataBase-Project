baec845d1f0e68b78c11bbc668062f6a5fa53b0c
Ting-An Hung <annhung323451@gmail.com>
2026-06-19 21:02:55 +0800

    fix env

--- data/db_data.sql
+++ data/db_data.sql
@@ -6,8 +6,7 @@ CREATE TABLE IF NOT EXISTS `Geolocation` (
     `geolocation_zip_code_prefix` VARCHAR(10) PRIMARY KEY,
     `geolocation_lat` DECIMAL(10, 8),
     `geolocation_lng` DECIMAL(11, 8),
-    `geolocation_city` VARCHAR(50),
-    `geolocation_state` VARCHAR(2)
+    `geolocation_city` VARCHAR(50)
 ) ENGINE = InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 
@@ -18,7 +17,6 @@ CREATE TABLE IF NOT EXISTS `Customers` (
     `customer_unique_id` VARCHAR(50) NOT NULL,
     `customer_zip_code_prefix` VARCHAR(10),
     `customer_city` VARCHAR(50),
-    `customer_state` VARCHAR(2),
     FOREIGN KEY (`customer_zip_code_prefix`) REFERENCES `Geolocation`(`geolocation_zip_code_prefix`)
 ) ENGINE = InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
@@ -28,7 +26,6 @@ CREATE TABLE IF NOT EXISTS `Sellers` (
     `seller_id` VARCHAR(50) PRIMARY KEY,
     `seller_zip_code_prefix` VARCHAR(10),
     `seller_city` VARCHAR(50),
-    `seller_state` VARCHAR(2),
     FOREIGN KEY (`seller_zip_code_prefix`) REFERENCES `Geolocation`(`geolocation_zip_code_prefix`)
 ) ENGINE = InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
@@ -98,7 +95,7 @@ CREATE TABLE IF NOT EXISTS `Order_Items` (
 CREATE TABLE IF NOT EXISTS `Order_Payments` (
     `order_id` VARCHAR(50),
     `payment_sequential` INT,
-    `payment_type` VARCHAR(20) CHECK (`payment_type` IN ('credit_card', 'voucher', 'debit_card', 'transfer')),
+    `payment_type` VARCHAR(20) CHECK (`payment_type` IN ('credit_card', 'cash', 'debit_card', 'transfer')),
     `payment_installments` INT,
     `payment_value` DECIMAL(10, 2),
     PRIMARY KEY (`order_id`, `payment_sequential`),
