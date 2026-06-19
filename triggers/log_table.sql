5ade408e18cd95876b240039258ea7ee80c8f52f
fionaliu0621 <52523171+fionaliu0621@users.noreply.github.com>
2026-06-19 18:03:54 +0800

    Remove order status log trigger and create log table
    Removed the trigger for logging order status changes and created a new table for logging.

--- triggers/log_table.sql
+++ triggers/log_table.sql
@@ -1,18 +1,8 @@
-USE railway;
-
-DELIMITER $$
-
-CREATE TRIGGER trg_order_status_log
-AFTER UPDATE ON Orders
-FOR EACH ROW
-BEGIN
-    IF OLD.order_status <> NEW.order_status THEN
-        INSERT INTO Order_Status_Log (
-            order_id, old_status, new_status
-        ) VALUES (
-            NEW.order_id, OLD.order_status, NEW.order_status
-        );
-    END IF;
-END$$
-
-DELIMITER ;
+CREATE TABLE `Order_Status_Log` (
+  `log_id` int NOT NULL AUTO_INCREMENT,
+  `order_id` varchar(50) DEFAULT NULL,
+  `old_status` varchar(20) DEFAULT NULL,
+  `new_status` varchar(20) DEFAULT NULL,
+  `changed_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`log_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=30 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
